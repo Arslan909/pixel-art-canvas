68ca964336a079f0875ccf078c3eeb8f724d8e4b
Arslan909 <arslanswl909@gmail.com>
2023-10-08 00:34:01 +0500

    added click and drag functionality to the board

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Pixel from "./components/Pixel";
 export default function App() {
   const number = 20;
   const [pixels, setPixels] = React.useState(Array(number * number).fill(false));
+  const [isDragging, setIsDragging] = React.useState(false);
+  const dragStartIndex = React.useRef(null);
 
   const handlePixelClick = (index) => {
     const updatedPixels = [...pixels];
@@ -12,17 +14,35 @@ export default function App() {
     setPixels(updatedPixels);
   };
 
+  const handleMouseDown = (index) => {
+    setIsDragging(true);
+    dragStartIndex.current = index;
+  };
+
+  const handleMouseEnter = (index) => {
+    if (isDragging) {
+      const updatedPixels = [...pixels];
+      updatedPixels[index] = !updatedPixels[index];
+      setPixels(updatedPixels);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    dragStartIndex.current = null;
+  };
 
   return (
-    <div className="grid-container">
-    {pixels.map((pixelState, index) => (
+    <div className="grid-container" onMouseUp={handleMouseUp}>
+      {pixels.map((pixelState, index) => (
         <Pixel
           key={index}
           pixelState={pixelState}
           onClick={() => handlePixelClick(index)}
+          onMouseDown={() => handleMouseDown(index)}
+          onMouseEnter={() => handleMouseEnter(index)}
         />
       ))}
-
     </div>
   );
 }
--- src/components/Pixel.jsx
+++ src/components/Pixel.jsx
@@ -2,11 +2,12 @@
 import PropTypes from 'prop-types';
 
 export default function Pixel(props) {
-  
   return (
     <div
       className={`pixel ${props.pixelState ? "black" : ""}`}
       onClick={props.onClick}
+      onMouseDown={props.onMouseDown} // Added onMouseDown event
+      onMouseEnter={props.onMouseEnter} // Added onMouseEnter event
     ></div>
   );
 }
@@ -14,4 +15,6 @@ export default function Pixel(props) {
 Pixel.propTypes = {
   pixelState: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
-};
+  onMouseDown: PropTypes.func.isRequired, 
+  onMouseEnter: PropTypes.func.isRequired
+}
