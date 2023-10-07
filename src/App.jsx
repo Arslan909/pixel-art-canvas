4070d23aef3affbeee2ad955867e5f6d6a63db3f
Arslan909 <arslanswl909@gmail.com>
2023-10-07 12:07:25 +0500

    added functioanlity to create pixels cells sturcture and add event listener to each cell

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,22 @@ export default function App() {
   const number = 20;
   const [pixels, setPixels] = React.useState(Array(number * number).fill(false));
 
-
+  const handlePixelClick = (index) => {
+    const updatedPixels = [...pixels];
+    updatedPixels[index] = !updatedPixels[index];
+    setPixels(updatedPixels);
+  };
   console.log(pixels)
 
   return (
     <div className="grid-container">
+    {pixels.map((pixelState, index) => (
+        <Pixel
+          key={index}
+          pixelState={pixelState}
+          onClick={() => handlePixelClick(index)}
+        />
+      ))}
 
     </div>
   );
