0b189284223e4b30ea14734c3e029751c0aa4241
Arslan909 <arslanswl909@gmail.com>
2023-10-13 02:10:01 +0500

    functionality to fixed the color filled pixels so they do not erase on clicking again

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ export default function App() {
   const number = 20;
   const [pixels, setPixels] = useState(Array(number * number).fill(false));
   const [isDragging, setIsDragging] = useState(false);
-  const [isInverted, setIsInverted] = useState(false); // State for color inversion
+  const [isInverted, setIsInverted] = useState(false); 
+  const [lockFilledBoxes, setLockFilledBoxes] = useState(false); 
+
 
 
   const handlePixelClick = (index) => {
+    if (!lockFilledBoxes || !pixels[index]) {
       const updatedPixels = [...pixels];
       updatedPixels[index] = !updatedPixels[index];
       setPixels(updatedPixels);
+    }
   };
 
-  const handleMouseDown = (index,event) => {
+  const handleMouseDown = (index, event) => {
     event.preventDefault();
     setIsDragging(true);
-    handleMouseEnter(index)
+    handleMouseEnter(index);
   };
 
   const handleMouseUp = () => {
@@ -39,22 +43,32 @@ export default function App() {
       if (isInverted) {
         updatedPixels[index] = !updatedPixels[index];
       } else {
-        updatedPixels[index] = !updatedPixels[index];
+        if (!lockFilledBoxes || !pixels[index]){
+          updatedPixels[index] = !updatedPixels[index];
+        }
       }
       setPixels(updatedPixels);
     }
   };
-  
+
   const handleInvertChange = () => {
     setIsInverted(!isInverted);
   };
 
+  const handleLockFilledBoxesChange = () => {
+    setLockFilledBoxes(!lockFilledBoxes);
+  };
+
   return (
-    <div className="container">
+    <div className="app-container">
       <div className="controls">
-        <label>
-          Invert Colors:   
-          <input type="checkbox" checked={isInverted} onChange={handleInvertChange}  className="checkBox"/>
+        <label className="invert-label">
+          Invert Colors:
+          <input type="checkbox" checked={isInverted} onChange={handleInvertChange} className="invert-checkbox" />
+        </label>
+        <label className="lock-label">
+          Lock Filled Boxes:
+          <input type="checkbox" checked={lockFilledBoxes} onChange={handleLockFilledBoxesChange} className="lock-checkbox" />
         </label>
       </div>
       <div className="grid-container">
